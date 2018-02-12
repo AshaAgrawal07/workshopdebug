0b903adcda402a5439f0746fb492eaabdb27d6c5
curtis.lowder <clowder2@illinois.edu>
2018-02-12 00:45:07 -0600

    Added a fix

--- src/cs126/codeworkshop/Frequency/StringUtil.java
+++ src/cs126/codeworkshop/Frequency/StringUtil.java
@@ -15,7 +15,7 @@ public class StringUtil {
 		System.out.println(mostFrequentChar("a"));
 		System.out.println(mostFrequentChar("aaaaaa"));
 		System.out.println(mostFrequentChar("aaabbb"));
-		System.out.println(mostFrequentChar("aaaabbb"));
+		System.out.println(mostFrequentChar("aaaaaaabbb"));
 		System.out.println(mostFrequentChar("she sells sea shells by the sea shore"));
 	}
 
@@ -45,10 +45,9 @@ public class StringUtil {
 		char maxChar = '-';
 		int maxCount = 0;
 		for (int i = 0; i < characterList.size(); i++) {
-			if (countList.get(i) > maxCount) {
+			if (countList.get(i) > maxCount)
 				maxChar = characterList.get(i);
 				maxCount = countList.get(i);
-			}
 		}
 
 		return maxChar;
@@ -56,9 +55,8 @@ public class StringUtil {
 
 	private static int findInList(List<Character> list, char c) {
 		for (int i = 0; i < list.size(); ++i) {
-			if (list.get(i) == c) {
+			if (list.get(i) == c)
 				return i;
-			}
 		}
 		return -1;
 	}
