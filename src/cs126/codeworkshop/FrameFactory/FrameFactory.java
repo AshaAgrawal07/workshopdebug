3eca965238acd8dc2daa331053562f4a475f8452
AshaAgrawal07 <ashaa2@illinois.edu>
2018-02-12 16:46:59 -0600

    frame factory complete

--- src/cs126/codeworkshop/FrameFactory/FrameFactory.java
+++ src/cs126/codeworkshop/FrameFactory/FrameFactory.java
@@ -31,13 +31,13 @@ public class FrameFactory {
 	public static String framerCreator(int n) {
 		String frame = "";
 		for (int i = 0; i < n; i++) {
-			if (i == 0 || i == n) {
+			if (i == 0 || i == n - 1) {
 				for (int j = 0; j < n; j++) {
 					frame += "#";
 				}
 			} else {
 				frame += "#";
-				for (int j = 0; j < n; j++) {
+				for (int j = 1; j < n - 1; j++) {
 					frame += " ";
 				}
 				frame += "#";
